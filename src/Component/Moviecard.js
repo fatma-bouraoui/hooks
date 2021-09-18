b0b6156c00f6cf9c1057fdf3932441671e286c74
fatma-bouraoui <bsfatmabr@gmail.com>
2021-09-18 15:41:42 +0200

    first commit

---
+++ src/Component/Moviecard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+import ReactStars from 'react-rating-stars-component';
+export const Moviecard = ({movie}) => {
+    return (
+        <div>
+            <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={movie.posterurl} />
+  <Card.Body>
+    <Card.Title>{movie.name}</Card.Title>
+    <Card.Text>
+      {movie.description}
+    </Card.Text>
+  </Card.Body>
+  <ReactStars
+    count={5}
+    size={24}
+    edit={false}
+    value={movie.rating}
+    activeColor="#ffd700"
+  />
+ 
+</Card>
+ 
+        </div>
+    )
+}
